a9547269b80a144f28ec4f5705ac16875d29d2b1
Cameron Guilbeau <cameroncarlg@gmail.com>
2024-08-24 22:25:30 -0700

    Add files via upload

--- home.nix
+++ home.nix
@@ -2,8 +2,8 @@
 
 {
   # Home Manager needs to know where to manage
-  home.username = "cameron";
-  home.homeDirectory = "/Users/cameron";
+  home.username = "nixos";
+  home.homeDirectory = "/home/nixos";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
@@ -46,13 +46,10 @@
     nix-prefetch-github # TBD
     nix-prefetch
     nh # Yet another Nix helper
-    m-cli # TBD
+    # m-cli # TBD
     # broot # Terminal File System navigator
     gh # GitHub CLI
     tig # Text mode interface for git
-    nodejs_22 # Nodejs
-    python3 # Python3
-    vscodium # Opensource Non-Telemetrey VSCode
     nerdfonts # NerdFonts
     fira-code # FiraCode
     lazydocker
@@ -85,11 +82,6 @@
       nix-prefetch-github-latest-release --meta -v $1 $2
     '')
   ];
-
-  programs.starship = {
-    enable = true;
-    enableFishIntegration = true;
-  };
 
   programs.broot = {
     enable = true;
@@ -133,40 +125,16 @@
     };
   };
 
-  programs.git = {
-    enable = true;
-    userName = "cameroncarlg";
-    userEmail = "cameroncarlg@gmail.com";
-  };
-
   programs.helix = {
     enable = true;
     defaultEditor = true;
     languages = {
       # the language-server option currently requires helix from the master branch at https://github.com/helix-editor/helix/
-      language-server.typescript-language-server = with pkgs.nodePackages; {
-        command = "${typescript-language-server}/bin/typescript-language-server";
-        args = [ "--stdio" "--tsserver-path=${typescript}/lib/node_modules/typescript/lib" ];
-      };
-      language-server.html-ls = with pkgs.nodePackages; {
-        command = "${vscode-html-languageserver-bin}/bin/vscode-html-languageserver";
-        args = [ "--stdio" ];
-        env = {
-          PATH = "${pkgs.nodePackages.vscode-html-languageserver-bin}/bin:${pkgs.nodePackages.nodejs}/bin:${pkgs.coreutils}/bin:$PATH";
-        };
-      };
-      language-server.json-ls = with pkgs.nodePackages; {
-        command = "${vscode-json-languageserver-bin}/bin/vscode-json-languageserver-bin";
-        args = [ "--stdio" ];
-        env = {
-          PATH = "${pkgs.nodePackages.vscode-json-languageserver-bin}/bin:${pkgs.nodePackages.nodejs}/bin:${pkgs.coreutils}/bin:$PATH";
-        };
-      };
+      # language-server.typescript-language-server = with pkgs.nodePackages; {
+      #   command = "${typescript-language-server}/bin/typescript-language-server";
+      #   args = [ "--stdio" "--tsserver-path=${typescript}/lib/node_modules/typescript/lib"];
+      # };
       language = [
-        {
-          name = "rust";
-          auto-format = false;
-        }
         {
           name = "html";
           formatter = {
@@ -204,17 +172,19 @@
         }
         {
           name = "tsx";
-          formatter = {
-            command = "${pkgs.nodePackages.prettier}/bin/prettier";
-            args = [ "--stdin-filepath" "{}" "--parser" "tsx" ];
-          };
+          auto-format = true;
+          # formatter = {
+          #   command = "${pkgs.nodePackages.prettier}/bin/prettier";
+          #   args = [ "--stdin-filepath" "{}" "--parser" "tsx" ];
+          # };
         }
         {
           name = "json";
-          formatter = {
-            command = "${pkgs.nodePackages.prettier}/bin/prettier";
-            args = [ "--stdin-filepath" "{}" "--parser" "json" ];
-          };
+          auto-format = true;
+          # formatter = {
+          #   command = "${pkgs.nodePackages.prettier}/bin/prettier";
+          #   args = [ "--stdin-filepath" "{}" "--parser" "json" ];
+          # };
         }
       ];
     };
@@ -223,6 +193,7 @@
       editor = {
         line-number = "relative";
         lsp.display-messages = true;
+        auto-format = true;
       };
       keys.normal = {
         esc = [ "collapse_selection" "keep_primary_selection" ];
@@ -244,15 +215,20 @@
   programs.fish = {
     enable = true;
     shellAbbrs = {
-      sw = "home-manager switch";
+      sw = "sudo nixos-rebuild switch";
       wthr = "wthrr -u f,12h,in";
       nf = "fastfetch";
-      ll = "br -sdp";
+      cat = "bat";
+      ld = "lazydocker";
+      lg = "lazygit";
+      # ll = "br -sdp";
     };
     functions = {
       trash = "mv $argv[1] $HOME/.Trash";
-      hxc = "sudo hx /Users/cameron/.config/home-manager/home.nix";
+      hxc = "sudo hx /etc/nixos/home.nix";
+      hxcv = "sudo hx /etc/nixos/configuration.nix";
       code = "codium";
+      cd = "builtin cd $argv; and ls";
     };
     interactiveShellInit = "
       set fish_greeting
@@ -298,27 +274,27 @@
       config.use_fancy_tab_bar = false
       config.show_new_tab_button_in_tab_bar = false
       config.window_background_opacity = 0.925
-      config.macos_window_background_blur = 30
       config.mouse_wheel_scrolls_tabs = true
       config.font = wezterm.font("Fira Code")
       config.enable_scroll_bar = true
+      config.hide_mouse_cursor_when_typing = false
 
       config.keys = {
-        { key = 'Enter', mods = 'CMD|SHIFT', action = wezterm.action.SplitHorizontal {domain = 'CurrentPaneDomain' }},
-        { key = 'Enter', mods = 'CMD', action = wezterm.action.SplitVertical {domain = 'CurrentPaneDomain' }},
-        { key = 't', mods = 'CMD|SHIFT', action = wezterm.action.ActivateTabRelative(-1) },
-        { key = 'w', mods = 'CMD', action = wezterm.action.CloseCurrentPane {confirm = true }},
-        { key = 'w', mods = 'CMD|SHIFT', action = wezterm.action.CloseCurrentTab {confirm = true }},
-        { key = 'j', mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Next' },
-        { key = 'k', mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Prev' },
-        { key = 'h', mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Left' },
-        { key = 'l', mods = 'CMD', action = wezterm.action.ActivatePaneDirection 'Right' },
-        { key = 'h', mods = 'CMD|SHIFT', action = wezterm.action.AdjustPaneSize { 'Left', 5 }},
-        { key = 'j', mods = 'CMD|SHIFT', action = wezterm.action.AdjustPaneSize { 'Down', 5 }},
-        { key = 'k', mods = 'CMD|SHIFT', action = wezterm.action.AdjustPaneSize { 'Up', 5 }},
-        { key = 'l', mods = 'CMD|SHIFT', action = wezterm.action.AdjustPaneSize { 'Right', 5 }},
-        { key = 'n', mods = 'CMD', action = wezterm.action.TogglePaneZoomState },
-        { key = 'p', mods = 'CMD', action = wezterm.action.ToggleFullScreen },
+        { key = 'Enter', mods = 'ALT', action = wezterm.action.SplitHorizontal {domain = 'CurrentPaneDomain' }},
+        { key = 'Enter', mods = 'ALT|SHIFT', action = wezterm.action.SplitVertical {domain = 'CurrentPaneDomain' }},
+        { key = 't', mods = 'ALT|SHIFT', action = wezterm.action.ActivateTabRelative(-1) },
+        { key = 'w', mods = 'ALT', action = wezterm.action.CloseCurrentPane {confirm = true }},
+        { key = 'w', mods = 'ALT|SHIFT', action = wezterm.action.CloseCurrentTab {confirm = true }},
+        { key = 'j', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Next' },
+        { key = 'k', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Prev' },
+        { key = 'h', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Left' },
+        { key = 'l', mods = 'ALT', action = wezterm.action.ActivatePaneDirection 'Right' },
+        { key = 'h', mods = 'ALT|SHIFT', action = wezterm.action.AdjustPaneSize { 'Left', 5 }},
+        { key = 'j', mods = 'ALT|SHIFT', action = wezterm.action.AdjustPaneSize { 'Down', 5 }},
+        { key = 'k', mods = 'ALT|SHIFT', action = wezterm.action.AdjustPaneSize { 'Up', 5 }},
+        { key = 'l', mods = 'ALT|SHIFT', action = wezterm.action.AdjustPaneSize { 'Right', 5 }},
+        { key = 'n', mods = 'ALT', action = wezterm.action.TogglePaneZoomState },
+        { key = 'p', mods = 'ALT|SHIFT', action = wezterm.action.ToggleFullScreen },
       }
       return config
     '';
